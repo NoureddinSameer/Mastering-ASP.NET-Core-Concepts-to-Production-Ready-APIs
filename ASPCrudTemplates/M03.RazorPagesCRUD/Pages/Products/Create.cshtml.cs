4d647919774d66b8998ee1c6434a32d3d9b88c5d
Noureddin Sameer <adx2187@gmail.com>
2025-10-29 14:27:00 +0300

    CRUD Across Templates(MVC, RazorPages,  BlazorSSR)

---
+++ ASPCrudTemplates/M03.RazorPagesCRUD/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,19 @@
+using M03.RazorPagesCRUD.Models;
+using M03.RazorPagesCRUD.Store;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace M03.RazorPagesCRUD.Pages;
+
+public class CreateModel(ProductStore store) : PageModel
+{
+    [BindProperty]
+    public Product Product { get; set; } = new();
+    public IActionResult OnPost()
+    {
+        Product.Id = Guid.NewGuid();
+        store.Add(Product);
+        return RedirectToPage("Index");
+    }
+
+}
---
+++ ASPCrudTemplates/M03.RazorPagesCRUD/Pages/Products/index.cshtml.cs
@@ -0,0 +1,17 @@
+using M03.RazorPagesCRUD.Models;
+using M03.RazorPagesCRUD.Store;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace M03.RazorPagesCRUD.Pages;
+
+public class IndexModel(ProductStore store) : PageModel
+{
+    public IEnumerable<Product> Products { get; set; } = [];
+    
+    public void OnGet()
+    {
+        Products = store.GetAll();
+    }
+}
+
