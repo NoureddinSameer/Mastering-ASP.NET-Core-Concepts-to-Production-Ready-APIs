9b6b7e2ef72aa1cb5a1c8f20ef3f7bb465ef6e98
Noureddin Sameer <adx2187@gmail.com>
2025-11-11 07:51:51 +0200

    I registered the service to enable versioning
    Inside Program.cs file

--- APIVersioning/M05.UrlPathVersionionMinimal/Program.cs
+++ APIVersioning/M05.UrlPathVersionionMinimal/Program.cs
@@ -1,4 +1,5 @@
 
+using Asp.Versioning;
 using M05.UrlPathVersionionMinimal.Data;
 using M05.UrlPathVersionionMinimal.Endpoints;
 
@@ -7,6 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<ProductRepository>();
 
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = new MediaTypeApiVersionReader("v");
+});
 
 var app = builder.Build();
 
