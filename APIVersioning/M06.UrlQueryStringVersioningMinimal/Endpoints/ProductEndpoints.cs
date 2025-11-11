bbea19e1181885ddbfc293d2c928f63c0317e323
Noureddin Sameer <adx2187@gmail.com>
2025-11-11 16:53:51 +0200

    Minimal Based APIs(Minimal API Query-String Versioning)
    Without Versioning

---
+++ APIVersioning/M06.UrlQueryStringVersioningMinimal/Endpoints/ProductEndpoints.cs
@@ -0,0 +1,30 @@
+
+using M06.UrlQueryStringVersioningMinimal.Data;
+using M06.UrlQueryStringVersioningMinimal.Responses.V1;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace M06.UrlQueryStringVersioningMinimal.Endpoints;
+
+public static class ProductEndpoints
+{
+    public static RouteGroupBuilder MapProductEndpoints(this IEndpointRouteBuilder app)
+    {
+         var productApi = app.MapGroup("api/products");
+
+        productApi.MapGet("{productId:guid}", GetProductById).WithName(nameof(GetProductById));
+
+        return productApi;
+    }
+
+    private static Results<Ok<ProductResponse>, NotFound<string>> GetProductById(
+        Guid productId,
+        ProductRepository repository, HttpResponse response)
+    {
+        var product = repository.GetProductById(productId);
+
+        if (product is null)
+            return TypedResults.NotFound($"Product with Id '{productId}' not found");
+
+        return TypedResults.Ok(ProductResponse.FromModel(product));
+    }
+}
