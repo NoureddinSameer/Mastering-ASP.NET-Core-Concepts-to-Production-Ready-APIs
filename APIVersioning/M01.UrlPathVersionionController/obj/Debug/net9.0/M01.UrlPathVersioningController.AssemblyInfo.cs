a207304eb6005c22db7a9fb128a73b424e0456d2
Noureddin Sameer <adx2187@gmail.com>
2025-11-11 04:25:47 +0200

    I add the package to apply versioning
    run this in terminal dotnet add package Microsoft.AspNetCore.Mvc.Versioning

--- APIVersioning/M01.UrlPathVersionionController/obj/Debug/net9.0/M01.UrlPathVersioningController.AssemblyInfo.cs
+++ APIVersioning/M01.UrlPathVersionionController/obj/Debug/net9.0/M01.UrlPathVersioningController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("M01.UrlPathVersioningController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6850679749fb3c30812544299543ff4920c0ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("M01.UrlPathVersioningController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("M01.UrlPathVersioningController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
