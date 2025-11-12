c344f7f91a4444cd9b1be5bc1aa6f6894be41ed4
Noureddin Sameer <adx2187@gmail.com>
2025-11-12 19:47:25 +0200

    EFCoreCodeFirst
    store data in memory

---
+++ DataPersistence/M01.EFCoreCodeFirst/Program.cs
@@ -0,0 +1,21 @@
+
+using M01.EFCoreCodeFirst.Data;
+using M01.EFCoreCodeFirst.Endpoints;
+using Microsoft.AspNetCore.Http.Json;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ProductRepository>();
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault;
+});
+
+
+var app = builder.Build();
+
+app.MapProductEndpoints();
+
+app.Run();
