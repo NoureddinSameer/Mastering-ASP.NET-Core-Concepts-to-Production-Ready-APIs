aadc0264d9cf5d58eb8df79cd9957a6af919caef
Noureddin Sameer <adx2187@gmail.com>
2025-11-13 06:02:16 +0200

    Add initial migration
    I add this package to use migration
    
    dotnet add package Microsoft.EntityFrameworkCore.Design --version 9.0.5
    
    Then I run this commands
    
    dotnet nuget locals all --clear
    
    dotnet tool install --global dotnet-ef --version 9.0.5
    
    dotnet ef --version
    
    dotnet ef migrations add InitialCreate --output-dir Data/Migrations
    
    dotnet ef database update

--- DataPersistence/M01.EFCoreCodeFirst/Program.cs
+++ DataPersistence/M01.EFCoreCodeFirst/Program.cs
@@ -2,6 +2,7 @@
 using M01.EFCoreCodeFirst.Data;
 using M01.EFCoreCodeFirst.Endpoints;
 using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,10 @@ builder.Services.Configure<JsonOptions>(options =>
     options.SerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault;
 });
 
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseSqlite("Data Source = app.db");
+});
 
 var app = builder.Build();
 
