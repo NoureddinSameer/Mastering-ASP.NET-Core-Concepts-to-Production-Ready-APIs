bc831ff233e8d341d1c3897e5a148142941d7c5f
Noureddin Sameer <adx2187@gmail.com>
2025-11-15 05:30:05 +0200

    Implementing Unit of Work
    Add Configuration of EF Core into Program.cs

---
+++ DataPersistence/M05.UnitOfWork/Program.cs
@@ -0,0 +1,28 @@
+
+using M05.UnitOfWork.Data;
+using M05.UnitOfWork.Endpoints;
+using M05.UnitOfWork.Interfaces;
+using M05.UnitOfWork.Repositories;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault;
+});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite("Data Source = app.db");
+});
+
+var app = builder.Build();
+
+app.MapProductEndpoints();
+
+app.Run();
