3659cb99778c2a438e9f1169655d39b019a5ab7e
Noureddin Sameer <adx2187@gmail.com>
2025-11-20 14:27:40 +0200

    Global Filter Registrations
    Update Program.cs file

--- FilterPipline/M01.ActionFilters/Program.cs
+++ FilterPipline/M01.ActionFilters/Program.cs
@@ -1,6 +1,12 @@
+using M01.ActionFilters.Filters;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<SampleActionFilter>(); // Global Filter Registrations
+});
 
 var app = builder.Build();
 
