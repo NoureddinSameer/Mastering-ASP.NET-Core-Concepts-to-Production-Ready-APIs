cf6feecd72b5098ccc0db4b398a2df8882829461
Noureddin Sameer <adx2187@gmail.com>
2025-11-21 04:04:43 +0200

    Apply TenantValidationFilter on a specific Action
    Update ProductController.cs file

--- FilterPipline/M02.ResourceFilters/Controllers/ProductController.cs
+++ FilterPipline/M02.ResourceFilters/Controllers/ProductController.cs
@@ -1,16 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using M02.ResourceFilters.Filters;
 
 namespace M02.ResourceFilters.Controllers;
 
 [ApiController]
 [Route("api/products")]
+
 public class ProductController() : ControllerBase
 {
 
     [HttpGet]
+    [ServiceFilter(typeof(TenantValidationFilter))]
     public IActionResult Get()
     {
         return Ok(new[] { "Keyboard [$52.99]", "Mouse, [$34.99]" });
+    }
+
+    [HttpGet]
+    [Route("get2")]
+    public IActionResult Get2()
+    {
+        return Ok(new[] { "Keyboard2 [$52.99]", "Mouse2, [$34.99]" });
     }
 }
 
