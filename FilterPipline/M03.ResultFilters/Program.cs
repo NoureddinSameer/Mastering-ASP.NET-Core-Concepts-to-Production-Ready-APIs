38a56ca856b8994fb15fb9c8edcd93f87b477a2e
Noureddin Sameer <adx2187@gmail.com>
2025-11-22 14:16:35 +0200

    Remove  Global Filter Registrations
    Update Program.cs

--- FilterPipline/M03.ResultFilters/Program.cs
+++ FilterPipline/M03.ResultFilters/Program.cs
@@ -5,8 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(options =>
 {
-    options.Filters.Add<EnvelopeResultFilter>();
+    // options.Filters.Add<EnvelopeResultFilter>();
 });
+
+builder.Services.AddScoped<EnvelopeResultFilter>();
 
 var app = builder.Build();
 
