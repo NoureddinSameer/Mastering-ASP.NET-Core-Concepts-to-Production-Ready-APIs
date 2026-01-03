7426ec167c917569f863a687d5d115f5107e567e
Noureddin Sameer <adx2187@gmail.com>
2026-01-03 15:59:30 +0200

    M06.GlobalErrorHandling

---
+++ ErrorHandling/M06.GlobalErrorHandling/Program.cs
@@ -0,0 +1,15 @@
+
+using M06.GlobalErrorHandling.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.MapErrorEndpoints();
+
+app.Run();
