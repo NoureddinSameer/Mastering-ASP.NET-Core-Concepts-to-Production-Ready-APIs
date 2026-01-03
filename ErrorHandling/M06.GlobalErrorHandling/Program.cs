48cb7ef1d7abc56d63a7b6b3f508531890714c41
Noureddin Sameer <adx2187@gmail.com>
2026-01-03 19:02:59 +0200

    Update Program.cs file
    Apply RFC9457

--- ErrorHandling/M06.GlobalErrorHandling/Program.cs
+++ ErrorHandling/M06.GlobalErrorHandling/Program.cs
@@ -5,8 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
+
+app.UseStatusCodePages();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.MapControllers();
 
