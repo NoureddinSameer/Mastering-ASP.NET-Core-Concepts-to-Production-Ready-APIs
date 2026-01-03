d5e4bb6f17b59a9b0014b45d6b699ac78d04939e
Noureddin Sameer <adx2187@gmail.com>
2026-01-03 04:39:06 +0200

    Update Program.cs
    Before adding this line you will see data without RFC9457
    if you send a request like this:
    
    ### Server Error
    GET {{baseUrl}}/api/minimal-fake-errors/server-error
    Content-Type: application/json
    
    After adding this line you will see data with RFC9457
    
    Note: we are in Development Environment
    Now

--- ErrorHandling/M05.MinimalRFC9457/Program.cs
+++ ErrorHandling/M05.MinimalRFC9457/Program.cs
@@ -3,7 +3,10 @@ using M05.MinimalRFC9457.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
+
 
 app.MapErrorEndpoints();
 
