9d5e0592f20c374cf9ab499b1049fd8be424c2b5
Noureddin Sameer <adx2187@gmail.com>
2026-01-17 21:56:58 +0200

    Add ApiController
    Now we have: 
    
    data Anotation 
    Model binding
    
    because we use ApiController

--- ModelValidations/M01.ControllerDataAnnotations/Controllers/ProductController.cs
+++ ModelValidations/M01.ControllerDataAnnotations/Controllers/ProductController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace M01.ControllerDataAnnotations.Controllers;
 
-// [ApiController]
+[ApiController]
 [Route("api/products")]
 public class ProductController : ControllerBase
 {
@@ -11,8 +11,9 @@ public class ProductController : ControllerBase
     [HttpPost]
     public IActionResult Post([FromBody]CreateProductRequest request)
     {
-        if(!ModelState.IsValid)
-            return ValidationProblem(ModelState);
+        // No need if [ApiController] is present
+        // if(!ModelState.IsValid)
+        //     return ValidationProblem(ModelState);
         return Created($"/api/products/{Guid.NewGuid()}", request);
     }
 }
