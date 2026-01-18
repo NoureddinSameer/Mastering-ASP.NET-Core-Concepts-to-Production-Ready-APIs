e7af42f34db6a9cc2f45de12b0e27a862e36aeb2
Noureddin Sameer <adx2187@gmail.com>
2026-01-18 21:01:00 +0200

    M02.MinimalDataAnnotations
    No Data annotation is appled until now

--- ModelValidations/M02.MinimalDataAnnotations/Validators/LaunchDateValidator.cs
+++ ModelValidations/M02.MinimalDataAnnotations/Validators/LaunchDateValidator.cs
@@ -4,12 +4,11 @@ namespace M02.MinimalDataAnnotations.Validators;
 
 public static class LaunchDateValidator
 {
-    public static ValidationResult? MustBeTodayOrFuture(DateTime datetime, ValidationContext validationContext)
+    public static ValidationResult? MustBeTodayOrFuture(DateTime datetime)
     {
         if (datetime.Date >= DateTime.UtcNow.Date)
             return ValidationResult.Success;
 
-        return new ValidationResult("Launch date must be today or in the future",
-        [validationContext.MemberName ?? "LaunchDate"]);
+        return new ValidationResult("Launch date must be today or in the future");
     }
 }
