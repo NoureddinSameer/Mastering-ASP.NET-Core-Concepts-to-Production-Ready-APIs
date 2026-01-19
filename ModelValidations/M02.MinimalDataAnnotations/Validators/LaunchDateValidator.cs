c2e030e458b5babc9c448b20484728c966ec6ef7
Noureddin Sameer <adx2187@gmail.com>
2026-01-19 19:49:46 +0200

    Activate Data Annotation
    non-generic way

--- ModelValidations/M02.MinimalDataAnnotations/Validators/LaunchDateValidator.cs
+++ ModelValidations/M02.MinimalDataAnnotations/Validators/LaunchDateValidator.cs
@@ -4,11 +4,12 @@ namespace M02.MinimalDataAnnotations.Validators;
 
 public static class LaunchDateValidator
 {
-    public static ValidationResult? MustBeTodayOrFuture(DateTime datetime)
+    public static ValidationResult? MustBeTodayOrFuture(DateTime datetime, ValidationContext validationContext)
     {
         if (datetime.Date >= DateTime.UtcNow.Date)
             return ValidationResult.Success;
 
-        return new ValidationResult("Launch date must be today or in the future");
+        return new ValidationResult("Launch date must be today or in the future",
+        [validationContext.MemberName ?? "LaunchDate"]);
     }
 }
