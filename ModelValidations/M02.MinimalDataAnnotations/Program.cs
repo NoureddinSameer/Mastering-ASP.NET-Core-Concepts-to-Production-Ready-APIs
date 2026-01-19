05d07027b6e1ab98ee3303755983f3f1b6d3cd37
Noureddin Sameer <adx2187@gmail.com>
2026-01-19 21:55:53 +0200

    Activate Data Annotation
    Generic way

--- ModelValidations/M02.MinimalDataAnnotations/Program.cs
+++ ModelValidations/M02.MinimalDataAnnotations/Program.cs
@@ -3,6 +3,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using M02.MinimalDataAnnotations.Requests;
+using M02.MinimalDataAnnotations.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,39 +18,6 @@ var app = builder.Build();
 app.MapPost("/api/products", (CreateProductRequest? request) =>
 {
     return Results.Created($"/api/products/{Guid.NewGuid()}", request);
-}).AddEndpointFilter(async (context,next) =>
-{
-    var argument = context.Arguments.OfType<CreateProductRequest>().FirstOrDefault();
-
-    if(argument is null)
-    {
-        return Results.Problem(new ProblemDetails
-        {
-            Title = "Bad Request",
-            Status = StatusCodes.Status400BadRequest,
-            Detail = $"{nameof(CreateProductRequest)} is null"
-        });
-    }
-    List<ValidationResult> validationResults = [];
-
-    var isValid = Validator.TryValidateObject(
-                            argument,
-                            new ValidationContext(argument),
-                            validationResults,
-                            true);
-    if (!isValid)
-    {
-        var errorGroup = validationResults
-            .SelectMany(v=> (v.MemberNames.Any() ? v.MemberNames: new [] { "" })
-            .Select(name => new { name, v.ErrorMessage }))
-            .GroupBy(x => x.name)
-            .ToDictionary(
-                g => g.Key,
-                g => g.Select(x => x.ErrorMessage!).ToArray()
-            );
-        return Results.ValidationProblem(errorGroup);
-    }
-    return await next(context);
-});
+}).Validate<CreateProductRequest>();
 
 app.Run();
