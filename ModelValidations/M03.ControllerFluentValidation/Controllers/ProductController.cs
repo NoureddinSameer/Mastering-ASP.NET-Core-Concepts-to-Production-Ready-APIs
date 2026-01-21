d43c4d0bb615a81e44f33960dde7452e1de7b818
Noureddin Sameer <adx2187@gmail.com>
2026-01-21 21:35:38 +0200

    M03.ControllerFluentValidation
    Until now we don't apply Fluent Validation

---
+++ ModelValidations/M03.ControllerFluentValidation/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+using M01.BasicSetup.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace M03.ControllerFluentValidation.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductController : ControllerBase
+{
+
+    [HttpPost]
+    public IActionResult Post(CreateProductRequest request)
+    {
+        // No need if [ApiController] is present
+        // if(!ModelState.IsValid)
+        //     return ValidationProblem(ModelState);
+        return Created($"/api/products/{Guid.NewGuid()}", request);
+    }
+}
