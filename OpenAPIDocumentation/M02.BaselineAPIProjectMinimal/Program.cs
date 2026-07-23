84ac4b252544367100cc6e91051fb6fba8426f77
Noureddin Sameer <adx2187@gmail.com>
2026-07-23 15:51:05 +0300

    Add SwaggerUI and Scaler
    Write this in the terminal to apply Scalar UI:
    
    dotnet add package Scalar.AspNetCore --version 2.4.7
    
    Write this in the terminal to apply SwaggerUI UI:
    
    dotnet add package Swashbuckle.AspNetCore.SwaggerUI --version 8.1.2
    
    After run this into terminal:
    
    dotnet run
    
    we can go to the browser:
    
    https://localhost:7979/scalar
    
    https://localhost:7979/swagger

--- OpenAPIDocumentation/M02.BaselineAPIProjectMinimal/Program.cs
+++ OpenAPIDocumentation/M02.BaselineAPIProjectMinimal/Program.cs
@@ -2,6 +2,7 @@ using Asp.Versioning;
 using M02.BaselineAPIProjectMinimal;
 using M02.BaselineAPIProjectMinimal.Data;
 using M02.BaselineAPIProjectMinimal.Endpoints;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,6 +32,18 @@ app.MapTokenEndpoints();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Project API V1");
+        options.SwaggerEndpoint("/openapi/v2.json", "Project API V2");
+
+        options.EnableDeepLinking();
+        options.DisplayRequestDuration();
+        options.EnableFilter();
+    });
+
+    app.MapScalarApiReference();
 }
 
 
